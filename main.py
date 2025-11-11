6701f18c6ab2c616e30dc8e32833ee7d90b42cc2
JackPearsonAtUAL <j.pearson0220251@arts.ac.uk>
2025-11-11 11:58:33 +0000

    Update main.py
    Added comments to explain code better

--- main.py
+++ main.py
@@ -30,29 +30,34 @@ def MessageBorder(user, text):
         Code from Bunyk on Stack Overflow
         ref: https://stackoverflow.com/questions/20756516/python-create-a-text-border-with-dynamic-size
         """
-        lines = text.splitlines()
-        width = max(len(s) for s in lines)
-        res = ['    * ' + '-' * (width-2) + ' *']
+        lines = text.splitlines()# split the inputted text into list
+        width = max(len(s) for s in lines)# make the texbox width equal to the largest line in the "lines" list
+        res = ['    * ' + '-' * (width-2) + ' *']# creates an array and the top border for the text, with an indent to denote the main user
+
+        # loop iterates through each entry in the "lines" list
         for s in lines:
-            res.append('    │' + (s + ' ' * width)[:width] + '│')
-        res.append('    * ' + '-' * (width-2) + ' *')
-        return '\n'.join(res)
+            res.append('    │' + (s + ' ' * width)[:width] + '│')# puts text inside the border, with an indent to denote the main user
+        res.append('    * ' + '-' * (width-2) + ' *')# creates the bottom border for the text, with an indent to denote the main user
+        return '\n'.join(res) # sends back the list as a joined string using new line to seperarte them
     else:
-        lines = text.splitlines()
-        width = max(len(s) for s in lines)
-        res = ['+ ' + '-' * (width-2) + ' +']
+        lines = text.splitlines()# split the inputted text into list
+        width = max(len(s) for s in lines)# make the texbox width equal to the largest line in the "lines" list
+        res = ['+ ' + '-' * (width-2) + ' +']# creates an array and the top border for the text
+        
+        # loop iterates through each entry in the "lines" list
         for s in lines:
-            res.append('│' + (s + ' ' * width)[:width] + '│')
-        res.append('+ ' + '-' * (width-2) + ' +')
-        return '\n'.join(res)
+            res.append('│' + (s + ' ' * width)[:width] + '│')# puts text inside the border
+        res.append('+ ' + '-' * (width-2) + ' +')# creates the bottom border for the text
+        return '\n'.join(res)# sends back the list as a joined string using new line to seperarte them
 
 def MessageInput(messaging):
     while messaging == True:
-        #get input
-        s = input("")
         
-        ident = s.split()
-        user = ident[0]
+        s = input("")# get input
+
+        
+        ident = s.split()# put the inputted text into a list of words
+        user = ident[0]# gets the user ID
         
         """
         Check the identifier is using usr, 
@@ -66,20 +71,31 @@ def MessageInput(messaging):
         #identifier is 0 for left, anything else for the right
         if user == "usr0" and len(ident) > 1:
             if ident[1] != " " and ident[1] != "":
-                del ident[0]
+                del ident[0]# removes the ID from the eventual string
+                """
+                outputs the text using the MessageBorder() function, 
+                with the user denoting the format
+                and ConcatMessage(ident) gives the text
+                """
                 print(MessageBorder(user, ConcatMessage(ident)))
             else:
-                messaging = False
+                messaging = False # ends messaging loop
                 print("exit")
         elif len(ident) > 1:
             if ident[1] != " " and ident[1] != "":
-                del ident[0]
+                del ident[0]# removes the ID from the eventual string
+                """
+                outputs the text using the MessageBorder() function, 
+                with the user denoting the format
+                and ConcatMessage(ident) gives the text
+                """
                 print(MessageBorder(user, ConcatMessage(ident)))
-            else:
+            else:               
+                messaging = False # ends messaging loop
                 print("exit")
-                messaging = False
-        
-        MessageInput(messaging)
+                
+        if messaging == True:
+            MessageInput(True)# repeat the texting function
 
 print("To print a message you must prefix it with\n'usr'+usernumber then enter a space")
 MessageInput(corresponding)
